e3eff16bb6ce594f76005f876e1d7b27e260f505
Omid-Shamkhali <omid.shamkhali@gmail.com>
2026-02-27 17:42:51 +0100

    Complete mobile menu, dark/light mode, and submenu functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,11 @@
 const toggleThemeBtn = document.querySelectorAll(".toggle-theme");
+const toggleSubmenuBtn=document.querySelector("#submenu-btn")
+const submenuTag=document.querySelector('.submenu')
+const menubarBtn=document.querySelector('.bar-btn')
+const mobileNav=document.querySelector('#mobile-menu')
+const closeMneuBtn=document.querySelector('.close-menu')
+const overlayItem=document.querySelector('.overlay')
+
 
 toggleThemeBtn.forEach(btn=>{
     btn.addEventListener("click" , () => {
@@ -10,4 +17,31 @@ toggleThemeBtn.forEach(btn=>{
             localStorage.setItem("theme" , "dark");
         }
     })
+})
+
+toggleSubmenuBtn.addEventListener('click',(e)=>{
+    
+    toggleSubmenuBtn.classList=" w-4 h-4  cursor-pointer"
+    if(submenuTag.classList.contains("hidden")){
+        submenuTag.classList.remove("hidden")
+        submenuTag.classList="flex flex-col gap-y-3 pl-8 mt-3 text-sm text-zinc-600 dark:text-white"
+    }else{
+        submenuTag.classList="hidden"
+        toggleSubmenuBtn.classList=" w-4 h-4  cursor-pointer rotate-180 transition-all"
+
+    }
+})
+
+menubarBtn.addEventListener('click',()=>{
+    mobileNav.classList.toggle('-left-64')
+  mobileNav.classList.toggle('left-0')
+  overlayItem.classList.toggle('hidden')
+})
+
+
+closeMneuBtn.addEventListener('click',()=>{
+    mobileNav.classList.toggle('left-0')
+    mobileNav.classList.toggle('-left-64')
+    overlayItem.classList.toggle('hidden')
+  
 })
