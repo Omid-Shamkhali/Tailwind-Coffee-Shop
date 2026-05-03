351dc173c76c4c46f0e890834f964279e5802626
Omid-Shamkhali <omid.shamkhali@gmail.com>
2026-05-03 22:02:24 +0200

    Hide delete icon when product quantity is greater than 1

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -3,12 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
   
     plusButtons.forEach(btn => {
       btn.addEventListener('click', () => {
-        const container = btn.closest('div'); 
+        const container = btn.closest('div');
+  
         const totalProduct = container.querySelector('.TotalProductNumber');
+        const trashIcon = container.querySelector('.trash-symbol');
+        const minusSymbol=container.querySelector('.minus-symbol')
   
         let count = parseInt(totalProduct.textContent);
         count++;
         totalProduct.textContent = count;
+  
+        if (count > 1 && trashIcon) {
+          trashIcon.style.display = 'none';
+          minusSymbol.style.display='block'
+        }
+      
       });
     });
   });
