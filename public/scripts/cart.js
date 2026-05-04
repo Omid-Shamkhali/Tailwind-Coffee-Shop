bdd229f0aab2a2136661357801418bc29d7b5d94
Omid-Shamkhali <omid.shamkhali@gmail.com>
2026-05-04 10:26:25 +0200

    feat(cart): add quantity increment/decrement and toggle trash/minus icons

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -7,17 +7,45 @@ document.addEventListener('DOMContentLoaded', () => {
   
         const totalProduct = container.querySelector('.TotalProductNumber');
         const trashIcon = container.querySelector('.trash-symbol');
-        const minusSymbol=container.querySelector('.minus-symbol')
+        const minusSymbol = container.querySelector('.minus-symbol');
   
         let count = parseInt(totalProduct.textContent);
         count++;
         totalProduct.textContent = count;
   
-        if (count > 1 && trashIcon) {
+        if (count > 1) {
           trashIcon.style.display = 'none';
-          minusSymbol.style.display='block'
+          minusSymbol.style.display = 'block';
+        } else {
+          trashIcon.style.display = 'block';
+          minusSymbol.style.display = 'none';
         }
-      
+      });
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const minusButtons = document.querySelectorAll('.minus-symbol');
+  
+    minusButtons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        const container = btn.closest('div');
+  
+        const totalProduct = container.querySelector('.TotalProductNumber');
+        const trashIcon = container.querySelector('.trash-symbol');
+        const minusSymbol = container.querySelector('.minus-symbol');
+  
+        let count = parseInt(totalProduct.textContent);
+  
+        if (count > 1) {
+          count--;
+          totalProduct.textContent = count;
+        }
+  
+        if (count === 1) {
+          trashIcon.style.display = 'block';
+          minusSymbol.style.display = 'none';
+        }
       });
     });
   });
