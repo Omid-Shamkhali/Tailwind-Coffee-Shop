acb86dba98ab9a8a9d798cf15aef916a84b0847c
Omid-Shamkhali <omid.shamkhali@gmail.com>
2026-05-05 21:26:32 +0200

    feat(cart): remove product from cart on trash icon click

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const plusButtons = document.querySelectorAll('.plus-number');
-  
     plusButtons.forEach(btn => {
       btn.addEventListener('click', () => {
         const container = btn.closest('div');
-  
         const totalProduct = container.querySelector('.TotalProductNumber');
         const trashIcon = container.querySelector('.trash-symbol');
         const minusSymbol = container.querySelector('.minus-symbol');
-  
         let count = parseInt(totalProduct.textContent);
         count++;
         totalProduct.textContent = count;
-  
         if (count > 1) {
           trashIcon.style.display = 'none';
           minusSymbol.style.display = 'block';
@@ -30,22 +26,30 @@ document.addEventListener('DOMContentLoaded', () => {
     minusButtons.forEach(btn => {
       btn.addEventListener('click', () => {
         const container = btn.closest('div');
-  
         const totalProduct = container.querySelector('.TotalProductNumber');
         const trashIcon = container.querySelector('.trash-symbol');
         const minusSymbol = container.querySelector('.minus-symbol');
-  
         let count = parseInt(totalProduct.textContent);
-  
         if (count > 1) {
           count--;
           totalProduct.textContent = count;
         }
-  
         if (count === 1) {
           trashIcon.style.display = 'block';
           minusSymbol.style.display = 'none';
         }
       });
     });
+  });
+
+ 
+
+  document.addEventListener('DOMContentLoaded', () => { 
+    const trashIcon = document.querySelectorAll('.trash-symbol');
+    trashIcon.forEach(btn => {
+        btn.addEventListener('click',()=>{
+            const product = btn.closest('.productCartHolder');
+            product.remove();
+        })
+    })
   });
